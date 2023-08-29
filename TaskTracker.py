8fe91a42f3f3e501bb47a010b33e016664643abc
Ashneet <ashneetnagi21@gmail.com>
2023-08-29 18:18:17 -0400

    sorted the tasks by importance

--- TaskTracker.py
+++ TaskTracker.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-habits = []
+habits = [[],[],[]]
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,30 +12,35 @@ def index():
         habit = request.form.get('habit')
         importance = request.form.get('importance')
         if habit:
-            habits.append({'name': habit, 'completed': False, 'importance': importance})
+            if importance == "box-red":
+                habits[0].append({'name': habit, 'completed': False, 'importance': importance})
+            elif importance == "box-yellow":
+                habits[1].append({'name': habit, 'completed': False, 'importance': importance})
+            else:
+                habits[2].append({'name': habit, 'completed': False, 'importance': importance})
     completion_percentage = calculateHabitPercentage(habits)   
     return render_template('index.html', habits=habits , completion_percentage = completion_percentage)
 
-@app.route('/complete/<int:habit_index>')
-def complete_habit(habit_index):
+@app.route('/complete/<int:list_index>/<int:habit_index>')
+def complete_habit(list_index,habit_index):
     if 0 <= habit_index < len(habits):
-        habits[habit_index]['completed'] = not (habits[habit_index]['completed'])
+        habits[list_index][habit_index]['completed'] = not (habits[list_index][habit_index]['completed'])
         completion_percentage = calculateHabitPercentage(habits)
         render_template('index.html', habits=habits, completion_percentage=completion_percentage)
     return redirect(url_for('index'))
 
 def calculateHabitPercentage(habits):
-    total_habits = len(habits)
-    completed_habits = sum(1 for habit in habits if habit['completed'])
+    total_habits = len(habits[0]) + len(habits[1]) + len(habits[2])
+    completed_habits = completed_habits = sum(1 for habit_list in habits for habit in habit_list if habit['completed'])
     if total_habits > 0: 
         completion_percentage = int((completed_habits / total_habits) * 100)
     else: completion_percentage = 0
     return completion_percentage
 
-@app.route('/delete/<int:habit_index>')
-def delete_habit(habit_index):
+@app.route('/delete/<int:list_index>/<int:habit_index>')
+def delete_habit(list_index,habit_index):
     if 0 <= habit_index < len(habits):
-        del habits[habit_index]
+        del habits[list_index][habit_index]
     return redirect(url_for('index'))
 
 
